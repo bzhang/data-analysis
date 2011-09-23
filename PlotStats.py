bfd3b3bed43bc9fd5e4a5307d10be43bb20f318a
Bingjun Zhang <bingjunzhang@gmail.com>
2011-09-22 23:24:51 -0500

    ADD: PlotStats.py for ploting

--- PlotStats.py
+++ PlotStats.py
@@ -1,5 +1,6 @@
 #! /usr/local/bin/python
 import os, glob, struct, pickle
+import numpy as np
 
 def save_data():
 	global fitness_pop, mutator_strength_pop, n_dele_pop, n_bene_pop
@@ -21,6 +22,13 @@ def restore_data():
 	n_dele_pop = data['n_dele_pop']
 	n_bene_pop = data['n_bene_pop']
 
+def string_to_float(nested_list):
+	result = map(list,[[]]*len(nested_list))	
+	for i in range(0,len(nested_list)):
+		for j in range(0,len(nested_list[i])):
+			result[i].append(float(nested_list[i][j]))
+	return result
+
 # data_path = "/Volumes/BigTwins/MutatorModelData/"
 # output_path = "/Volumes/BigTwins/Dropbox/MutatorModel/Results/"
 data_path = "/Users/bingjun/Documents/MutatorModel/"
@@ -30,40 +38,11 @@ dirs = glob.glob("MutCount_*_G5000*_MutaMR0.0_*")
 print(os.getcwd())
 print(len(dirs))
 
-n_gen = 5000
-# counter = len(dirs)
-for dir_name in dirs:
-# if counter > 0:
-	os.chdir(dir_name)
-	print(dir_name)	
-	files = glob.glob("*.txt")
-	# n_file = len(files)
-	fitness_pop = map(list,[[]]*n_gen)
-	mutator_strength_pop = map(list,[[]]*n_gen)
-	n_dele_pop = map(list,[[]]*n_gen)
-	n_bene_pop = map(list,[[]]*n_gen)
-	for file_name in files:
-	# if n_file > 0:
-		file = open(file_name,'rb')
-		print(file_name)
-		file.readline()
-		records = [record for record in file.read().split('\n') if record]
-		for i in range(0,n_gen):
-			# print(fitness_pop[i])
-			record = records[i].split()
-			fitness_pop[i].append(record[1])
-			mutator_strength_pop[i].append(record[3])
-			n_dele_pop[i].append(record[5])
-			n_bene_pop[i].append(record[7])
-		file.close()
-		# print(fitness_pop)
-		# n_file -= 1
-	# counter -= 1		
-# else:
-# 	break
-	save_data()
-	os.chdir("..")
+os.chdir(dirs[0])
+restore_data()
 
-print(fitness_pop[1])
-	
-	
+fitness_pop = string_to_float(fitness_pop)
+mutator_strength_pop = string_to_float(mutator_strength_pop)
+n_dele_pop = string_to_float(n_dele_pop)
+n_bene_pop = string_to_float(n_bene_pop)
+			
