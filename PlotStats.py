db118f1a2d7ecda978aa2496931a044b30f2cb29
Bingjun Zhang <bingjunzhang@gmail.com>
2011-09-22 19:19:19 -0500

    ADD: read data into nested lists; bug exists

--- PlotStats.py
+++ PlotStats.py
@@ -21,17 +21,35 @@ for dir_name in dirs:
 		for file_name in files:
 			if n_file > 0:
 				file = open(file_name,'rb')
+				n_gen = sum(1 for line in file) - 1
+				print(n_gen)
+				fitness_pop = map(list,[[]]*n_gen)
+				mutator_strength_pop = map(list,[[]]*n_gen)
+				n_dele_pop = map(list,[[]]*n_gen)
+				n_bene_pop = map(list,[[]]*n_gen)
 				print(file_name)
-				for i in range(1,11):
-					print(file.readline())
+				header = file.readline()
+				print(header)
+				record_format = 'hdddddddd'
+				record_size = struct.calcsize(record_format)				
+				for i in range(0,10):
+					record = file.read(record_size)					
+					print(record)
+					fitness_pop[i].append(record[1])
+					mutator_strength_pop[i].append(record[3])
+					n_dele_pop[i].append(record[5])
+					n_bene_pop[i].append(record[7])
 				file.close()
+				print(fitness_pop)
 				n_file -= 1
 		counter -= 1
 	else:
 		break
 
+save_data()
+
 def save_data():
-	globle fitness_pop, mutator_strength_pop, n_dele_pop, n_bene_pop
+	global fitness_pop, mutator_strength_pop, n_dele_pop, n_bene_pop
 	file = open("state",'w')
 	data = {'fitness_pop':fitness_pop, 
 			'mutator_strength_pop':mutator_strength_pop, 
@@ -41,7 +59,7 @@ def save_data():
 	file.close()
 	
 def restore_data():
-	globle fitness_pop, mutator_strength_pop, n_dele_pop, n_bene_pop
+	global fitness_pop, mutator_strength_pop, n_dele_pop, n_bene_pop
 	file = open("state",'r')
 	data = pickle.load(file)
 	file.close()
