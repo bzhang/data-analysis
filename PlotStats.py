fbd852814d14473ee8ac805b4385efd0fd672d89
Bingjun Zhang <bingjunzhang@gmail.com>
2011-09-29 23:02:10 -0500

    BUG: fix regular expression codes

--- PlotStats.py
+++ PlotStats.py
@@ -11,18 +11,22 @@ import util
 data_path = "/Users/bingjun/Documents/MutatorModel/"
 output_path = "/Users/bingjun/Dropbox/MutatorModel/Results/"
 os.chdir(data_path)
-dirs = glob.glob("MutCount_*_R0.0_*G5000*_MutaMR0.0*")
+dirs = glob.glob("MutCount_*G5000*_MutaMR0.0*")
 # dirs=glob.glob("MutCount*_R0.0_*_MutaMR0.0_*")
 print(os.getcwd())
 print(len(dirs))
 
-regexp=re.compile(r"DeleMR(?P<mu>[\d\.E-]+)_"
-				  r"_G(?P<nG>[\d]+)_")
+nG_exp=re.compile(r"_G(?P<nG>\d+)_")
+mu_exp=re.compile(r"DeleMR(?P<mu>[\d\.E-]+)_")
+
 for dir_name in dirs:
-	result = regexp.search(dir_name)
-	base_mu = float(result.group('mu'))
-	n_gen = int(result.group('nG'))
-	
+	nG = nG_exp.search(dir_name)
+	mu = mu_exp.search(dir_name)	
+	base_mu = float(mu.group('mu'))
+	n_gen = int(nG.group('nG'))
+	print(base_mu)
+	print(n_gen)
+	os.chdir(dir_name)
 	util.restore_mean_CI()
 	fig = plt.figure()
 	errorbar(range(1,n_gen+1),fitness_mean,fitness_CI,fmt='ro',ecolor='gray',capsize='0')
